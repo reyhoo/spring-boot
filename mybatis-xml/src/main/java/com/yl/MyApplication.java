b3f243d8875fb961965f1c5f2482c4a5faa3ef67
yl-mac <373561022@qq.com>
2019-05-02 20:32:02 +0800

    application和servletInitializer整合

--- mybatis-xml/src/main/java/com/yl/MyApplication.java
+++ mybatis-xml/src/main/java/com/yl/MyApplication.java
@@ -3,11 +3,19 @@ package com.yl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+/**
+ * Created by summer on 2017/5/8.
+ */
 @SpringBootApplication
 @MapperScan("com.yl.mapper")
-public class MyApplication {
-
+public class MyApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
